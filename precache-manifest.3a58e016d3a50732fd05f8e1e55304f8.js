8fcdb3d908f9e84cf72db2a42f14119af09642db
shreedhar desai <desaishreedhar09@gmail.com>
2020-06-05 21:57:40 +0530

    Updates

---
+++ precache-manifest.3a58e016d3a50732fd05f8e1e55304f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc01c03f51da5266e3d93f5fe38a7788",
+    "url": "/sudoku_puzzle/index.html"
+  },
+  {
+    "revision": "b804396a02cf7cf44463",
+    "url": "/sudoku_puzzle/static/css/main.b7325354.chunk.css"
+  },
+  {
+    "revision": "36b645024eff9441105c",
+    "url": "/sudoku_puzzle/static/js/2.706ad76d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sudoku_puzzle/static/js/2.706ad76d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b804396a02cf7cf44463",
+    "url": "/sudoku_puzzle/static/js/main.a06b7209.chunk.js"
+  },
+  {
+    "revision": "8382f95dce360065c828",
+    "url": "/sudoku_puzzle/static/js/runtime-main.624485a1.js"
+  }
+]);
